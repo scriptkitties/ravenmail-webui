9add7f9f279fea636f2b3f61848748fdf25b9f21
Patrick Spek <p.spek@tyil.nl>
2016-12-06 08:22:21 +0100

    Update domain edit view to use Pure

--- resources/views/domain/edit.blade.php
+++ resources/views/domain/edit.blade.php
@@ -2,15 +2,12 @@
 
 @section('content')
 <h2>Edit {{ $domain->name }}</h2>
-<form method="post" action="{{ route('domains.update', ['name' => $domain->name]) }}">
+<form class="pure-form pure-form-aligned" method="post" action="{{ route('domains.update', ['name' => $domain->name]) }}">
     {{ csrf_field() }}
     {{ method_field('PUT') }}
-    <div class="container">
-        <div class="grid">
-            <section class="grid__col grid__col--1-of-2 content-right">
-                Public domain
-            </section>
-            <section class="grid__col grid__col--1-of-2 content-left">
+    <fieldset>
+        <div class="pure-control-group">
+            <label class="pure-checkbox" for="public">
                 <input
                     type="checkbox"
                     name="public"
@@ -18,16 +15,14 @@
                         checked="checked"
                     @endif
                 >
-            </section>
+                Public domain
+            </label>
         </div>
-    </div>
-    <div class="container">
-        <section class="text-center">
-            <button type="submit">
-                <i class="fa fa-fw fa-save"></i>
-                Save
-            </button>
-        </section>
+    <div class="pure-control-group">
+        <button class="pure-button" type="submit">
+            <i class="fa fa-fw fa-save"></i>
+            Save
+        </button>
     </div>
 </form>
 @endsection
