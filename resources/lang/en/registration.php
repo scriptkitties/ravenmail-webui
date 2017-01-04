8d0f61b4d8ee3ea420808a84f777491945b8363b
Patrick Spek <p.spek@tyil.nl>
2017-01-04 19:50:35 +0100

    Add additional translation strings

--- resources/lang/en/registration.php
+++ resources/lang/en/registration.php
@@ -1,7 +1,9 @@
 <?php
 
 return [
+    'dupe' => 'The chosen email address is already taken.',
+    'illegal' => 'Your email address is illegal.',
+    'regex_error' => 'The regex engine failed to parse the chose email address. Check https://en.wikipedia.org/wiki/Email_address#Local-part for more information.',
     'success' => 'Your account, :account, has been registered. You can now use it to log in to the service.',
-    'dupe' => 'The chosen email address is already taken.'
 ];
 
