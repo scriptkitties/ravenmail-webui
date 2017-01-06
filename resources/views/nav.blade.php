07373b6537a5e47d8f69fecadb95d6caf75ed651
Patrick Spek <p.spek@tyil.nl>
2017-01-06 22:54:56 +0100

    Update nav to use the user route

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -7,7 +7,7 @@
             <li class="pure-menu-item"><a class="pure-menu-link" href="{{ route('logout') }}">logout</a></li>
         @else
             <li class="pure-menu-item"><a class="pure-menu-link" href="{{ route('login') }}">login</a></li>
-            <li class="pure-menu-item"><a class="pure-menu-link" href="{{ route('registration.create') }}">register</a></li>
+            <li class="pure-menu-item"><a class="pure-menu-link" href="{{ route('user.create') }}">register</a></li>
         @endif
     </ul>
 </nav>
