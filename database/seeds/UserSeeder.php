0999c50e8ac3186ddd4692a8ac3c1ae48cc7712a
Patrick Spek <p.spek@tyil.nl>
2017-01-18 08:43:57 +0100

    Update userseeder for easier testing

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -14,11 +14,51 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        // make sure to have an site admin
         DB::table('users')->insert([
             'local' => 'test',
             'domain' => 'test.test',
             'password' => bcrypt('test'),
             'admin' => true,
+            'active' => true,
+        ]);
+
+        // make sure to have a domain admin
+        DB::table('users')->insert([
+            'local' => 'admin',
+            'domain' => 'test.test',
+            'password' => bcrypt('test'),
+            'admin' => false,
+            'active' => true,
+        ]);
+
+        DB::table('domain_moderators')->insert([
+            'domain_id' => Domain::findByNameOrFail('test.test')->id,
+            'user_id' => User::findByAddressOrFail('admin@test.test')->id,
+            'admin' => true,
+        ]);
+
+        // make sure to have a domain moderator
+        DB::table('users')->insert([
+            'local' => 'mod',
+            'domain' => 'test.test',
+            'password' => bcrypt('test'),
+            'admin' => false,
+            'active' => true,
+        ]);
+
+        DB::table('domain_moderators')->insert([
+            'domain_id' => Domain::findByNameOrFail('test.test')->id,
+            'user_id' => User::findByAddressOrFail('mod@test.test')->id,
+            'admin' => false,
+        ]);
+
+        // make sure to have a normal user
+        DB::table('users')->insert([
+            'local' => 'user',
+            'domain' => 'test.test',
+            'password' => bcrypt('test'),
+            'admin' => false,
             'active' => true,
         ]);
 
