fce19181c24484a6d919b8e5baabe2f8b7c43e39
Patrick Spek <p.spek@tyil.nl>
2017-02-28 08:13:48 +0100

    Remove unused methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -110,17 +110,6 @@ class UserController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @return \Illuminate\Http\Response
@@ -159,16 +148,5 @@ class UserController extends Controller
         $user->save();
 
         return redirect()->route('dashboard');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        // NYI
     }
 }
